b3743162b0cc05ebb7690b95576bf0c6d6a62660
Øyvind Strømmen <insula.ventus@gmail.com>
2019-10-14 14:20:56 +0200

    Update FregItem to match the new data collector format

--- src/main/java/no/ssb/rawdata/converter/app/freg/FregItem.java
+++ src/main/java/no/ssb/rawdata/converter/app/freg/FregItem.java
@@ -5,35 +5,23 @@ import lombok.Builder;
 import lombok.Value;
 import no.ssb.rawdata.api.RawdataMessage;
 
-import java.util.TreeSet;
-
 @Builder
 @Value
 public class FregItem {
     private final ULID.Value ulid;
     private final String position;
-    private String personId;
     private byte[] eventXml;
     private byte[] personXml;
+    private byte[] metadataJson;
 
-    // TODO: Include info from the metadata section instead
     public static FregItem from(RawdataMessage rawdataMessage) {
-        TreeSet<String> keys = new TreeSet(rawdataMessage.keys());
-
-        String eventDocKey = keys.ceiling("event-document");
-        String personDocKey = keys.ceiling("person-document");
-        String personId = null;
-        if (personDocKey != null) {
-            personId = personDocKey.replace("person-document-", "").replace(".xml", "");
-        }
-
         return FregItem.builder()
-          .ulid(rawdataMessage.ulid())
-          .position(rawdataMessage.position())
-          .personId(personId)
-          .eventXml(rawdataMessage.get(eventDocKey))
-          .personXml(rawdataMessage.get(personDocKey))
-          .build();
+                .ulid(rawdataMessage.ulid())
+                .position(rawdataMessage.position())
+                .eventXml(rawdataMessage.get("event"))
+                .personXml(rawdataMessage.get("person"))
+                .metadataJson(rawdataMessage.get("manifest.json"))
+                .build();
     }
 
     public boolean hasPerson() {
@@ -45,6 +33,6 @@ public class FregItem {
     }
 
     public String toIdString() {
-        return String.format("%s (pos=%s, person=%s)", ulid, position, personId);
+        return String.format("%s (pos=%s)", ulid, position);
     }
 }
