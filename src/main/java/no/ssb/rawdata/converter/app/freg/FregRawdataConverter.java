81dd45c10837156f62a27bff52b00f5c1c175e4b
Øyvind Strømmen <insula.ventus@gmail.com>
2019-10-14 10:09:57 +0200

    Fix issue with empty parquet files
    The stream from which the records are read where being closed
    prematurely in a try-with-resources block.

--- src/main/java/no/ssb/rawdata/converter/app/freg/FregRawdataConverter.java
+++ src/main/java/no/ssb/rawdata/converter/app/freg/FregRawdataConverter.java
@@ -1,11 +1,10 @@
 package no.ssb.rawdata.converter.app.freg;
 
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import no.ssb.avro.convert.xml.XmlToRecords;
 import no.ssb.rawdata.api.RawdataMessage;
+import no.ssb.rawdata.converter.core.AbstractRawdataConverter;
 import no.ssb.rawdata.converter.core.AggregateSchemaBuilder;
-import no.ssb.rawdata.converter.core.RawdataConverter;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.generic.GenericRecordBuilder;
@@ -13,21 +12,12 @@ import org.apache.avro.generic.GenericRecordBuilder;
 import javax.inject.Singleton;
 import javax.xml.stream.XMLStreamException;
 import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 @Singleton
 @Slf4j
-public class FregRawdataConverter implements RawdataConverter {
+public class FregRawdataConverter extends AbstractRawdataConverter {
     private final Schema fregPersonSchema;
     private final Schema fregEventSchema;
     private final Schema aggregateSchema;
@@ -39,18 +29,12 @@ public class FregRawdataConverter implements RawdataConverter {
     private static final String DEFAULT_SCHEMA_FILE_EVENT = "schema/freg-event.avsc";
 
     public FregRawdataConverter(FregRawdataConverterConfig fregConverterConfig) {
-
-        try {
-            fregPersonSchema = new Schema.Parser().parse(loadSchema(fregConverterConfig.getSchemaFilePerson(), DEFAULT_SCHEMA_FILE_PERSON));
-            fregEventSchema = new Schema.Parser().parse(loadSchema(fregConverterConfig.getSchemaFileEvent(), DEFAULT_SCHEMA_FILE_EVENT));
-            aggregateSchema = new AggregateSchemaBuilder("no.ssb.dataset")
-              .schema(ELEMENT_NAME_FREG_PERSON, fregPersonSchema)
-              .schema(ELEMENT_NAME_FREG_EVENT, fregEventSchema)
-              .build();
-        }
-        catch (IOException e) {
-            throw new FregRawdataConverterException("Unable to locate avro schema. " + e.getMessage());
-        }
+        fregPersonSchema = readAvroSchema(fregConverterConfig.getSchemaFilePerson(), DEFAULT_SCHEMA_FILE_PERSON);
+        fregEventSchema = readAvroSchema(fregConverterConfig.getSchemaFileEvent(), DEFAULT_SCHEMA_FILE_EVENT);
+        aggregateSchema = new AggregateSchemaBuilder("no.ssb.dataset")
+                .schema(ELEMENT_NAME_FREG_PERSON, fregPersonSchema)
+                .schema(ELEMENT_NAME_FREG_EVENT, fregEventSchema)
+                .build();
     }
 
     @Override
@@ -61,61 +45,37 @@ public class FregRawdataConverter implements RawdataConverter {
     @Override
     public GenericRecord convert(RawdataMessage rawdataMessage) {
         log.trace("convert no.ssb.rawdata message {}", rawdataMessage);
+
         GenericRecordBuilder rootRecordBuilder = new GenericRecordBuilder(aggregateSchema);
         FregItem fregItem = FregItem.from(rawdataMessage);
+
         if (fregItem.hasPerson()) {
-            xmlToAvro(fregItem.getPersonXml(), ELEMENT_NAME_FREG_PERSON, fregPersonSchema).forEach(record ->
-              rootRecordBuilder.set(ELEMENT_NAME_FREG_PERSON, record)
-            );
-        }
-        else {
-            log.info("Missing person data for freg item {}", fregItem.toIdString());
+            xmlToAvro(fregItem.getPersonXml(), ELEMENT_NAME_FREG_PERSON, fregPersonSchema, rootRecordBuilder);
+        } else {
+            log.error("Missing person data for freg item {}", fregItem.toIdString());
         }
 
         if (fregItem.hasEvent()) {
-            xmlToAvro(fregItem.getEventXml(), ELEMENT_NAME_FREG_EVENT, fregEventSchema).forEach(record ->
-                rootRecordBuilder.set(ELEMENT_NAME_FREG_EVENT, record)
-            );
-        }
-        else {
-            log.info("Missing event data for freg item {}", fregItem.toIdString());
+            xmlToAvro(fregItem.getEventXml(), ELEMENT_NAME_FREG_EVENT, fregEventSchema, rootRecordBuilder);
+        } else {
+            log.error("Missing event data for freg item {}", fregItem.toIdString());
         }
 
         return rootRecordBuilder.build();
     }
 
-    Stream<GenericRecord> xmlToAvro(byte[] xmlData, String rootXmlElementName, Schema schema) {
+    private void xmlToAvro(byte[] xmlData, String rootXmlElementName, Schema schema, GenericRecordBuilder recordBuilder) {
         InputStream xmlInputStream = new ByteArrayInputStream(xmlData);
         try (XmlToRecords xmlToRecords = new XmlToRecords(xmlInputStream, rootXmlElementName, schema)) {
-            return StreamSupport.stream(xmlToRecords.spliterator(), false);
+            xmlToRecords.forEach(record -> recordBuilder.set(rootXmlElementName, record));
         } catch (XMLStreamException | IOException e) {
-            throw new FregRawdataConverterException("Error converting to XML", e);
-        }
-    }
-
-    @SneakyThrows
-    private InputStream loadSchema(Optional<String> schemaFile, String defaultSchemaFile) {
-        try {
-            if (schemaFile.isPresent()) {
-                return new FileInputStream(Paths.get(schemaFile.get()).toAbsolutePath().normalize().toFile());
-            }
-            else {
-                return getClass().getClassLoader().getResourceAsStream(defaultSchemaFile);
-            }
-        }
-        catch (FileNotFoundException e) {
-            throw new FregRawdataConverterException("Unable to locate avro schema: " + e.getMessage());
+            throw new FregRawdataConverterException("Could not convert Freg XML", e);
         }
     }
 
     static class FregRawdataConverterException extends RuntimeException {
-        public FregRawdataConverterException(String message) {
-            super(message);
-        }
-
-        public FregRawdataConverterException(String message, Throwable cause) {
+        FregRawdataConverterException(String message, Throwable cause) {
             super(message, cause);
         }
     }
-
 }
