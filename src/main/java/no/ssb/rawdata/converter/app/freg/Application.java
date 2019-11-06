02c7c4f6487f574196e91449dcf0d1c4b955d68f
Øyvind Strømmen <insula.ventus@gmail.com>
2019-11-06 14:32:59 +0100

    Log an exception at start up to test stacktrace formatting in kibana

--- src/main/java/no/ssb/rawdata/converter/app/freg/Application.java
+++ src/main/java/no/ssb/rawdata/converter/app/freg/Application.java
@@ -1,11 +1,20 @@
 package no.ssb.rawdata.converter.app.freg;
 
 import io.micronaut.runtime.Micronaut;
+import lombok.extern.slf4j.Slf4j;
 import no.ssb.rawdata.converter.core.util.EnvrionmentVariables;
 
+@Slf4j
 public class Application {
 
     public static void main(String[] args) {
+
+        try {
+            int i = 1 / 0;
+        } catch (Exception e) {
+            log.warn("Failed", new RuntimeException("Couldn't divide by zero", e));
+        }
+
         Micronaut.build(null)
           .mainClass(Application.class)
           .environmentVariableIncludes(EnvrionmentVariables.withPrefix("RAWDATA_CLIENT").toArray(new String[0]))
